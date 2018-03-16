872d709cad4adf10bcafc5b083164cef28cd6c36
mattsmuh <matthew@slateblank.com>
2018-03-16 19:25:15 +0000

    add bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_tenant!, :only => [ :index ]
+  skip_before_action :authenticate_tenant!, :only => [ :index ] # this line means don't have to be logges in to see index page
 
   def index
+    if current_user
+      if session[:tenant_id]
+        Tenant.set_current_tenant session[:tenant_id]
+      else
+        Tenant.set_current_tenant current_user.tenants.first
+      end
+      @tenant = Tenant.current_tenant
+      params[:tenant_id] = @tenant.id
+    end
   end
 end
